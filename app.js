ee4bebd9763e788d6cbc68ed7f905dfe0bd0238c
Loren Barrick <Lorenbarrick@gmail.com>
2015-08-28 13:37:58 -0700

    add how middleware functions work

--- app.js
+++ app.js
@@ -6,12 +6,12 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send('Hello, this is dog');
+// app.get('/', function(request, response) {
+  // response.send('Hello, this is dog');
   // same as
   //response.write('Hello, this is dog');
   //response.end();
-});
+// });
 app.get('/blocks', function(request, response) {
   var blocks = ['Fixed', 'Movable', 'Rotating'];
   //better syntax for JSON objects and arrays is
@@ -23,7 +23,31 @@ app.get('/blocks', function(request, response) {
   // 301 as the first argument in the function;
   response.redirect(301, '/parts');
 });
-app.listen(3000, function() {
-  console.log('Listening on port 3000');
-});
+// app.listen(3000, function() {
+//   console.log('Listening on port 3000');
+// });
 // curl http://localhost:3000/
+
+//section 2
+// app.get('/', function(request, response) {
+//   response.sendFile(__dirname + '/public/index.html');
+  // __dirname === name of the directory the currently executing script resides in.
+// });
+// using middleware to make the same call. Syntax as follows
+app.use(express.static('public'));
+// static middleware serving files from the public folder;
+app.listen(3000, function() {
+  console.log('listening on port 3000');
+});
+
+//executing middleware functions
+//middleware a
+// app.use(function(request, response, next) {
+//   ....
+//   next();
+// });
+// this continues until it reaches middleware D
+// app.use(function(request, response, next) {
+// ...
+// response.send('done!');
+// })
